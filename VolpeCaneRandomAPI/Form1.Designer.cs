794607fe29df7dc4a367d3295cb343a309e9a1a0
Simone Denardi <simone.denardi@lasportiva.com>
2021-08-27 11:17:25 +0200

    Aggiungere i file di progetto.

---
+++ VolpeCaneRandomAPI/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace VolpeCaneRandomAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btmVolpe = new System.Windows.Forms.Button();
+            this.btnCane = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnCane);
+            this.panel1.Controls.Add(this.btmVolpe);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 66);
+            this.panel1.TabIndex = 0;
+            // 
+            // btmVolpe
+            // 
+            this.btmVolpe.Location = new System.Drawing.Point(12, 12);
+            this.btmVolpe.Name = "btmVolpe";
+            this.btmVolpe.Size = new System.Drawing.Size(75, 34);
+            this.btmVolpe.TabIndex = 0;
+            this.btmVolpe.Text = "Volpe";
+            this.btmVolpe.UseVisualStyleBackColor = true;
+            this.btmVolpe.Click += new System.EventHandler(this.btmVolpe_Click);
+            // 
+            // btnCane
+            // 
+            this.btnCane.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCane.Location = new System.Drawing.Point(713, 12);
+            this.btnCane.Name = "btnCane";
+            this.btnCane.Size = new System.Drawing.Size(75, 34);
+            this.btnCane.TabIndex = 1;
+            this.btnCane.Text = "Cane";
+            this.btnCane.UseVisualStyleBackColor = true;
+            this.btnCane.Click += new System.EventHandler(this.btnCane_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 384);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCane;
+        private System.Windows.Forms.Button btmVolpe;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ VolpeCaneRandomAPI/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VolpeCaneRandomAPI
+{
+    public partial class Form1 : Form
+    {
+        HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("appliication/json"));
+
+        }
+
+        private async void btmVolpe_Click(object sender, EventArgs e)
+        {
+            string url = "https://randomfox.ca/floof/";
+
+            HttpResponseMessage risposta = await client.GetAsync(url);
+
+            if (risposta.IsSuccessStatusCode)
+            {
+                ImmagineVolpe immagine = await risposta.Content.ReadAsAsync<ImmagineVolpe>();
+                pictureBox1.ImageLocation = immagine.Image;
+            }
+            else
+            {
+                pictureBox1.ImageLocation = "https://upload.wikimedia.org/wikipedia/commons/f/f0/Error.svg";
+            }
+        }
+
+        private async void btnCane_Click(object sender, EventArgs e)
+        {
+            string url = "https://random.dog/woof.json";
+
+            HttpResponseMessage risposta = await client.GetAsync(url);
+
+            if (risposta.IsSuccessStatusCode)
+            {
+                ImmagineCane immagine = await risposta.Content.ReadAsAsync<ImmagineCane>();
+                pictureBox1.ImageLocation = immagine.Url;
+            }
+            else
+            {
+                pictureBox1.ImageLocation = "https://upload.wikimedia.org/wikipedia/commons/f/f0/Error.svg";
+            }
+        }
+    }
+}
